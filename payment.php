ca6149991100a91df440573d52f0186d58ca43b0
Steinweber <max@steinweber.info>
2014-07-28 20:15:28 +0200

    Update payment.php

--- payment.php
+++ payment.php
@@ -11,9 +11,9 @@
  	'currency' => $currency,
  	'mtid' => $mtid,
  	'merchantClientId' => '3256',
- 	'okUrl' => 'http://188.194.170.38/test.php?ok=1&mtid=' . $mtid .'&currency=' . $currency . '&amount=' . $amount,
- 	'nokUrl' => 'http://188.194.170.38/test.php?nok',
- 	'pnUrl' => 'http://188.194.170.38/test.php?pn=1&mtid=' . $mtid .'&currency=' . $currency . '&amount=' . $amount,);
+ 	'okUrl' => 'http://example.com/_onefile.php?ok=1&mtid=' . $mtid .'&currency=' . $currency . '&amount=' . $amount,
+ 	'nokUrl' => 'http://example.com/_onefile.php?nok',
+ 	'pnUrl' => 'http://example.com/_onefile.php?pn=1&mtid=' . $mtid .'&currency=' . $currency . '&amount=' . $amount,);
  $result = $payment->newPayment($transaction);
  if (!$result)
  {
